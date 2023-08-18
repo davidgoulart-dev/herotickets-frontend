f221df4dae3c85cb2e5d7e3d55b0f277789b9889
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-08-17 23:56:36 -0300

    Desenvolvendo página para adição de Eventos

---
+++ src/app/(pages)/create-event/page.tsx
@@ -0,0 +1,82 @@
+import { Input } from "@/app/components/Form/Input";
+import { categories } from "@/app/utils/categories";
+
+export default function CreateEvent() {
+    return (
+        <div className="container m-auto">
+            <div className="grid md:grid-cols-2 gap-1  grid-cols-1 p-8">
+                <div className="mb-4 pr-6 border-r-2 border-[#61D9DE]">
+                    <div className=" mb-4">
+                        <p className="text-blue text-2xl font-medium">Adicionar Eventos</p>
+                        <p className="text-blue text-base font-light">Crie o seu próprio evento da maneira que você preferir! :)</p>
+
+
+                    </div>
+                    <Input title="Titulo" placeholder="Insira o nome do seu evento" type="text" />
+                    <Input title="Endereço" placeholder="Insira o endereço do seu evento" type="text" />
+                    <Input title="Cupom" placeholder="Insira o endereço do seu evento" type="text" />
+                </div>
+                <div className="mb-4 ml-4 mr-3">
+                    <p className="text-blue text-2xl font-medium">Adicionar Eventos</p>
+                    <p className="text-blue text-base font-light">Crie o seu próprio evento da maneira que você preferir! :)</p>
+                    <div className=" my-4">
+                        <p className="text-blue text-base font-medium">Banner</p>
+                        <p className="text-neutral-500 text-sm font-light">Insira um banner no formato 336x280</p>
+                        <div className="w-full h-28 bg-zinc-300 rounded-3xl shadow">
+
+                        </div>
+
+                    </div>
+                </div>
+                <div>
+                    <div className="grid grid-cols-2 gap-3">
+
+                        <Input title="Data" placeholder="Insira o endereço do seu evento" type="date" />
+                        <Input title="Horário" placeholder="Insira o endereço do seu evento" type="number" />
+
+
+                    </div>
+                    <p className="text-blue text-base font-medium">Crie o seu próprio evento da maneira que você preferir! :)</p>
+
+
+                    <div className="grid grid-cols-4 gap-2  mt-4">
+                        {categories.map((category) => (
+                            (
+                                <div className="text-blue">
+                                    <input className=" mr-1" type="checkbox" />
+                                    <label htmlFor="">{category.name}</label>
+                                </div>
+                            )
+                        ))}
+
+
+
+                    </div>
+                    <div className=" mt-4">
+                        <p className="text-base font-medium text-blue">Valor</p>
+                        <p className="text-neutral-500 text-sm font-light">Caso seu evento seja gratuito, o campo deverá ficar vazio. Caso haja mais de um setor, basta adicionar a seção. Se houver cupom promocional, basta colocar o código no campo “cupom”.</p>
+
+                    </div>
+                    <div className="grid grid-cols-5 gap-2">
+                        <Input title="Preço" placeholder="R$ 0,00" type="text" className="col-span-2" />
+                        <Input title="Setor" placeholder="Insira o nome do setor" type="text" className="col-span-3" />
+
+
+                    </div>
+                    <Input title="Descrição" placeholder="Insira o nome do setor" type="textarea" className="col-span-3" />
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+
+
+
+    )
+
+
+}
---
+++ src/app/components/Form/Input.tsx
@@ -0,0 +1,47 @@
+import { ForwardRefRenderFunction, forwardRef} from 'react'
+
+interface IInput {
+    placeholder: string,
+    type: string,
+    title: string,
+    className?: string
+}
+export const InputBase: ForwardRefRenderFunction<HTMLInputElement & HTMLTextAreaElement, IInput > = (
+    {placeholder, type, className, title, ...rest },
+    ref,
+) => {
+    if(type === 'textarea'){
+        return (
+            <div>
+            <label>
+            {title}
+         </label> 
+         <textarea className={`w-full px-6 py-[5px] bg-white rounded-lg border border-teal-400 text-base`}  
+         
+         placeholder={placeholder}
+         ref={ref}
+         rows={5}
+         {...rest} 
+         />
+
+            </div>
+        )
+    }
+    return (
+        <div className={` mb-4 text-blue font-medium ${className}`}>
+            <label>
+               {title}
+            </label> 
+            <input className={`w-full px-6 py-[5px] bg-white rounded-lg border border-teal-400 text-base`}  
+            type={type}
+            placeholder={placeholder}
+            ref={ref}
+            {...rest} 
+            />
+       
+        </div>
+
+    )
+}
+
+export const Input = forwardRef(InputBase);
