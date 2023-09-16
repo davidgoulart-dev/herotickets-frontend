2927e5703b7214381a411c92e5558ed9dee8c161
David Goulart <davidgoulart.tae@gmail.com>
2023-09-15 21:30:15 -0300

    feat: componente FormWrapper desenvolvido

--- src/app/components/FormWrapper.tsx
+++ src/app/components/FormWrapper.tsx
@@ -1,13 +1,18 @@
+import { Button } from "./Form/Button"
+import { Input } from "./Form/Input"
+
 export const FormWrapper = () => {
     return (
-        <div>
+        <div className=" bg-gray-50 shadow rounded-3xl">
             <p className=" bg-blue rounded-3xl text-white text-center rounded-b-none p-3">Ingressos</p>
-            <div className=" flex justify-between items-center">
-                <div>
-                    <p className=" font-bold text-lg">Pista</p>
+            <div className=" p-6">
+                <div className=" flex justify-between items-center">
+                    <div>
+<p className=" font-bold text-lg">Pista</p>
                     <p>Entrada e acesso à pista do evento.</p>
-                </div>
-                {/* outro lado */}
+
+                    </div>
+                    
                 <div className=" flex">
                     <div className=" bg-blue rounded text-white font-medium w-8 h-8 flex justify-center items-center">
                         <p>-</p>
@@ -19,6 +24,19 @@ export const FormWrapper = () => {
                         <p>+</p>
                     </div>
                 </div>
+                
+                </div>
+                {/* outro lado */}
+                
+                <div className="grid grid-cols-5 gap-3 mt-3">
+                    
+                    <Input placeholder="Insira aqui um cupom de desconto" title="Cupom" className="col-span-3" />
+                    <Input placeholder="R$0,00" title="Subtotal" className="col-span-2" />
+
+                </div>
+                <Input placeholder="Insira seu nome" title="Nome"  />
+                <Input placeholder="Insira seu email" title="Email"  />
+                <Button title="Cadastrar"/>
 
             </div>
         </div>
